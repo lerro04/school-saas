340068483d4c3b99e8fc3a85f8ce48ef2bbf7bfa
lerro04 <lerrobvuure2004@gmail.com>
2026-06-13 11:12:14 +0200

    fix: remove syntax error in cors.php and update origin

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,12 +1,11 @@
 <?php
-
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://your-app.vercel.app'],,
+    'allowed_origins' => ['https://school-saas-dun.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 ];
